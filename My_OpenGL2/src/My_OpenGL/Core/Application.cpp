94cc4c9ea5dd99d8f9f16cc031b188c48656ad08
Your Name <a7skhy@gmail.com>
2020-02-21 21:36:26 +0545

    refactor for only spot light

--- My_OpenGL2/src/My_OpenGL/Core/Application.cpp
+++ My_OpenGL2/src/My_OpenGL/Core/Application.cpp
@@ -57,10 +57,10 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	//glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
 
-	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
