31578f6cb4a56cf27b758465cd6613d1da0e591f
Your Name <a7skhy@gmail.com>
2020-03-29 18:28:38 +0545

    ASSERT macro fixed

--- My_OpenGL2/src/My_OpenGL/Core/Core.h
+++ My_OpenGL2/src/My_OpenGL/Core/Core.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <iostream>
+#ifdef DEBUG
+	#define ENABLE_ASSERTS
+#endif
 
-#define ENABLE_ASSERTS 1
-// make a logging library first
-#if ENABLE_ASSERTS
+#ifdef ENABLE_ASSERTS
 	#define CORE_ASSERT(x, ...) { if(!(x)) { CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }}
-	#define ASSERT(x, ...) { if(!(x)) { ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }}
+	#define ASSERT(x, ...) { if(!(x)) { CLIENT_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }}
 #else
 	#define CORE_ASSERT(x, ...)
 	#define ASSERT(x, ...)
